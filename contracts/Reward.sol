5e71dddb2c6c475175db32c22d1c988dc4494307
Nawaga <projectnawaga@gmail.com>
2025-05-23 21:25:31 +0700

    update reward contract

--- contracts/Reward.sol
+++ contracts/Reward.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract Reward {
-    mapping(address => uint256) public balance;
+    mapping(address => uint256) public rewards;
 
-    function mint(address to, uint256 amount) external {
-        balance[to] += amount;
+    event RewardGiven(address indexed user, uint256 amount);
+
+    function giveReward(address user, uint256 amount) public {
+        rewards[user] += amount;
+        emit RewardGiven(user, amount);
+    }
+
+    function checkReward(address user) public view returns (uint256) {
+        return rewards[user];
     }
 }
